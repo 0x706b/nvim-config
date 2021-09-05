c547e6e3cbdd214e0f70ed95126a1581190fa941
0x706b <peter@peterkrol.com>
2021-09-05 12:22:07 -0400

    feat: better everforest colors

--- lua/colors/everforest.lua
+++ lua/colors/everforest.lua
@@ -1,17 +1,25 @@
+local config = vim.fn['everforest#get_configuration']()
+local palette = vim.fn['everforest#get_palette'](config.background)
+
 return {
-  bg = "#2b3339",
-  fg = "#d3c6aa",
-  dark_gray = "#323c41",
-  gray = "#5f6d67",
-  light_gray = "#d3c6aa",
-  light_red = "#e67e80",
-  dark_red = "#e67e80",
-  green = "#a7c080",
-  yellow = "#dbbc7f",
-  orange = "#e69875",
-  cyan = "#83c092",
-  blue = "#7fbbb3",
-  dark_purple = "#503946",
-  light_purple = "#d699b6",
-  info = '#68B9F0'
+  bg = palette.bg0[1],
+  bg0 = palette.bg1[1],
+  bg1 = palette.bg1[1],
+  bg2 = palette.bg3[1],
+  fg = palette.fg[1],
+  fg0 = palette.grey2[1],
+  fg1 = palette.grey1[1],
+  fg2 = palette.grey0[1],
+  b = {
+    red = palette.red[1],
+    yellow = palette.yellow[1],
+    blue = palette.blue[1]
+  },
+  n = {
+    yellow = palette.yellow[1],
+    red = palette.red[1],
+    blue = palette.blue[1],
+    green = palette.green[1],
+    magenta = palette.purple[1]
+  }
 }
