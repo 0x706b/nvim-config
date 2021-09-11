057aec8cc57c3a045433c656e54e2f81ee06b842
0x706b <peter@peterkrol.com>
2021-09-11 00:18:04 -0400

    feat: use local prettier if exists

--- prettier-eslint_d.sh
+++ prettier-eslint_d.sh
@@ -1,3 +1,9 @@
 filepath=$1
 
-prettier --stdin-filepath $filepath | eslint_d --stdin --stdin-filename $filepath --fix-to-stdout
+local_prettier=$PWD/node_modules/.bin/prettier
+
+if test -f $local_prettier; then
+  $local_prettier --stdin-filepath $filepath | eslint_d --stdin --stdin-filename $filepath --fix-to-stdout
+else
+  prettier --stdin-filepath $filepath | eslint_d --stdin --stdin-filename $filepath --fix-to-stdout
+fi
